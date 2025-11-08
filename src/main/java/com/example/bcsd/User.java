9b01d8a9db46b863a210ccc2e565e316d110b62e
Dohun-BackEnd <kkaauu871@gmail.com>
2025-11-08 15:05:07 +0900

    feat: User 클래스 생성

---
+++ src/main/java/com/example/bcsd/User.java
@@ -0,0 +1,19 @@
+package com.example.bcsd;
+
+public class User {
+    private int age;
+    private String name;
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
