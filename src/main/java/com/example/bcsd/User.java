b76f66334c18a7202e6b68654ea207d4b45aef22
Dohun-BackEnd <kkaauu871@gmail.com>
2025-11-08 21:13:56 +0900

    feat: User 클래스 id변수, 메소드 기능 추가

--- src/main/java/com/example/bcsd/User.java
+++ src/main/java/com/example/bcsd/User.java
@@ -1,19 +1,36 @@
 package com.example.bcsd;
 
 public class User {
+    private String id;
     private int age;
     private String name;
 
-    public User(int age, String name) {
+    public User(String id, int age, String name) {
+        this.id = id;
         this.age = age;
         this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public int getAge() {
         return age;
     }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
