4a8da8d1d1eda3b4f4cae42f7b6e271e84e254a0
Dohun-BackEnd <kkaauu871@gmail.com>
2025-11-08 22:35:28 +0900

    refactor: CRUD_API 클래스 POST 수정

--- src/main/java/com/example/bcsd/CRUD_API.java
+++ src/main/java/com/example/bcsd/CRUD_API.java
@@ -21,18 +21,18 @@ public class CRUD_API {
     }
 
     @GetMapping("/user/{id}")
-    public User user(@PathVariable("id")  String id) {
+    public User user(@PathVariable("id") String id) {
         return userMap.get(id);
     }
 
-    @PostMapping("/user{id}")
-    public void getUser(@PathVariable("id") String id,
-                        @RequestParam("age") int age,
-                        @RequestParam("name") String name) {
-        User user = new User(id, age, name);
+    @PostMapping("/user/{id}")
+    public void getUser(
+            @PathVariable("id") String id,
+            @RequestBody User user)
+        {
         userMap.put(id, user);
-    }
-    
+        }
+
     @PutMapping("/user/{id}")
     public void setUser(
             @PathVariable("id") String id,
