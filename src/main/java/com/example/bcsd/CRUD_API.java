772f1bb24a2b19efbc99c423d707e8b9626df12b
Dohun-BackEnd <kkaauu871@gmail.com>
2025-11-08 21:40:40 +0900

    feat: CRUD_API 클래스 PUT API 구현

--- src/main/java/com/example/bcsd/CRUD_API.java
+++ src/main/java/com/example/bcsd/CRUD_API.java
@@ -25,4 +25,14 @@ public class CRUD_API {
         return userMap.get(id);
     }
 
+    @PutMapping("/user/{id}")
+    public void setUser(
+            @PathVariable("id") String id,
+            @RequestParam("age") int age,
+            @RequestParam("name") String name)
+    {
+        User user = new User(id, age, name);
+        userMap.put(id, user);
+    }
+
 }
