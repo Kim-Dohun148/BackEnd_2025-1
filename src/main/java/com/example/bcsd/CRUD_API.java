91b93b62b43134f48638de3e6e624bdcda1bed91
Dohun-BackEnd <kkaauu871@gmail.com>
2025-11-08 21:35:38 +0900

    feat: CRUD_API 클래스 데이터 초기화 메소드 init 구현

--- src/main/java/com/example/bcsd/CRUD_API.java
+++ src/main/java/com/example/bcsd/CRUD_API.java
@@ -1,4 +1,22 @@
 package com.example.bcsd;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Controller
 public class CRUD_API {
+    private Map<String, User> userMap;
+
+    @PostConstruct
+    public void init() {
+        userMap = new HashMap<String, User>();
+        userMap.put("1", new User("1", 23, "김도훈"));
+        userMap.put("2", new User("2", 24, "도훈"));
+        userMap.put("3", new User("3", 25, "훈도김"));
+    }
 }
