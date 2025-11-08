2aaa9110ff8bd9914d9be390c57c2ec12ee3f076
Dohun-BackEnd <kkaauu871@gmail.com>
2025-11-08 15:06:28 +0900

    feat: json 요청 시 응답 반환 구현

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -20,11 +20,20 @@ public class HelloController {
     }
 
     @GetMapping("/introduce")
-    public String introduce() { return "introduce"; }
+    public String introduce() {
+        return "introduce";
+    }
 
     @ResponseBody
     @GetMapping(value = "/introduce", params = "name") // 파라미터 구분
     public String introduce2(@RequestParam("name") String myName) { // name 입력받기
         return "안녕하세요 제 이름은" + myName + "입니다!";
+    }
+
+    @ResponseBody
+    @GetMapping("/json")
+    public User json() {
+        User person = new User(23, "김도훈");
+        return person;
     }
 }
