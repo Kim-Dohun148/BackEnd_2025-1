fbce50b85482910b4ab3a2453aedbd9ea12fea64
Kim-Dohun <kkaauu871@gmail.com>
2025-11-15 23:54:06 +0900

    feat: Service 클래스 getArticles(특정 article 조회) 메서드 구현

--- src/main/java/com/example/bcsd/Controller/Service.java
+++ src/main/java/com/example/bcsd/Controller/Service.java
@@ -1,4 +1,12 @@
 package com.example.bcsd.Controller;
 
+import com.example.bcsd.Model.Article;
+import org.springframework.http.ResponseEntity;
+
 public class Service {
+    private Repository repository;
+
+    public ResponseEntity<Article> getArticles(String id) {
+        return repository.getArticles(id);
+    }
 }
